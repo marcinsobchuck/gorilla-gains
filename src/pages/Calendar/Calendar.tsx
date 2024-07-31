088ee1981957bc0b163c0a5afa5bde4347f9ea9a
Marcin <marcinsobchuck@gmail.com>
2024-07-31 19:35:34 +0200

    test 1

--- src/pages/Calendar/Calendar.tsx
+++ src/pages/Calendar/Calendar.tsx
@@ -1,12 +1,11 @@
 import { MainContentWrapper, Sidebar } from "@layouts/RootLayout/RootLayout.styled"
-import { HumanSilhouette } from "@pages/Dashboard/components/DashboardGrid/components/HumanSilhouette/HumanSilhouette"
 
 export const Calendar = () => {
   return (
     <>
       <Sidebar>Day info</Sidebar>
       <MainContentWrapper>
-        <HumanSilhouette />
+        <div>Test</div>
       </MainContentWrapper>
     </>
   )
