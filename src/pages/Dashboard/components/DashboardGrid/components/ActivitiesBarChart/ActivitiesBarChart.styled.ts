50d2e8d3ccc8c4134f9c3ce5ebf5d8f3597c34bc
Marcin <marcinsobchuck@gmail.com>
2025-01-13 21:12:51 +0100

    make bar chart display separate bar for specific activity type at given month

---
+++ src/pages/Dashboard/components/DashboardGrid/components/ActivitiesBarChart/ActivitiesBarChart.styled.ts
@@ -0,0 +1,33 @@
+import styled from "styled-components"
+
+import { FlexContainer } from "@components/FlexContainer/FlexContainer.styled"
+
+export const BarChartTooltipWrapper = styled(FlexContainer)`
+  width: 220px;
+  padding: 16px;
+  border-radius: 9px;
+  box-shadow: ${({ theme }) => theme.popperBoxShadow};
+  background-color: ${({ theme }) => theme.navBackgroundColor};
+`
+
+export const MonthWrapper = styled(FlexContainer)`
+  padding: 9px;
+  border-radius: 6px;
+  background-color: ${({ theme }) => theme.selectBackgroundColor};
+  font-size: 12px;
+  font-weight: 500;
+  margin-top: 18px;
+`
+
+export const ValueItem = styled(FlexContainer)`
+  font-weight: 600;
+  p {
+    text-transform: capitalize;
+    font-size: 12px;
+  }
+
+  span {
+    font-size: 12px;
+    color: ${({ theme }) => theme.secondary};
+  }
+`
